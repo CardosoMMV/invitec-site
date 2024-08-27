d80f7dd84e93b777c9c7f9b84b816f936f567142
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-08-27 18:08:31 -0300

    Create dropdown menu

--- programação/menu_toggle.js
+++ programação/menu_toggle.js
@@ -2,3 +2,11 @@ function clickMenu() {
     const menuLista = document.querySelector("#menuLista")
     menuLista.classList.toggle("ocultado")
 }
+function solucoesDropDown() {
+    const menuDropdown = document.querySelector("#solucoesDropDown")
+    menuDropdown.classList.toggle("escondeDropDown")
+}
+function casesDropDown() {
+    const menuDropdown = document.querySelector("#casesDropDown")
+    menuDropdown.classList.toggle("escondeDropDown")
+}
