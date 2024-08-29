1185bc469c9698adcfa38cc076bdd6acc8141126
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-08-29 20:41:04 -0300

    ajustes de imagem

--- programação/menu_toggle.js
+++ programação/menu_toggle.js
@@ -4,9 +4,20 @@ function clickMenu() {
 }
 function solucoesDropDown() {
     const menuDropdown = document.querySelector("#solucoesDropDown")
+    const chevronRight = document.querySelector("#escondeDropDown__solucoes-chevron-right")
     menuDropdown.classList.toggle("escondeDropDown")
+    chevronRight.classList.toggle("escondeDropDown__solucoes-chevron-right-actived")
 }
+let controlador = 0
 function casesDropDown() {
     const menuDropdown = document.querySelector("#casesDropDown")
+    const chevronRight = document.querySelector("#escondeDropDown__cases-chevron-right")
     menuDropdown.classList.toggle("escondeDropDown")
+    if(controlador == 0){
+        chevronRight.style.transform = `rotate(${90}deg)`
+        controlador = 1
+    }else{
+        chevronRight.style.transform = `rotate(${0}deg)`
+        controlador = 0
+    }
 }
