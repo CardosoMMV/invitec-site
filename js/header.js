3e931b52e78eef696f083f83f3f6381bb655c4f0
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-09-06 16:05:10 -0300

    menu hamburguer-dropdown responsive

--- js/header.js
+++ js/header.js
@@ -1,24 +1,16 @@
-const btnMobile = document.querySelector('.Header-Main__Bnt');
-let static = window.pageYoffset
-
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  document.querySelector('.Header-Main__Nav').classList.toggle('Header-Main__Nav--Active');
-  btnMobile.classList.toggle('Header-Main__Bnt--active');
-  scrollTo(0, static)
-}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-//função para abrir o dropdown
-function OpenDropdown(element){
-    let Dropdown = element.target;
-    let WordText = Dropdown.parentNode;
-    WordText = WordText.children[1];
-
-    WordText.classList.toggle("dropDown-Options--active");
-    WordText.classList.toggle("dropDown-Options--disable");
-}
-
-document.querySelectorAll(".dropDown").forEach(element => {element.addEventListener('click', OpenDropdown, true)});
+const btn_menu = document.querySelector('#btn-menu')
+const navbar = document.querySelector('.navbar')
+const solucoes_item = document.querySelector('#solucoes-item')
+const cases_item = document.querySelector('#cases-item')
+btn_menu.addEventListener('click', e => {
+  btn_menu.classList.toggle('navbar__btn-menu--actived')
+  navbar.classList.toggle('navbar--actived')
+})
+solucoes_item.addEventListener('click', e => {
+  e.preventDefault()
+  solucoes_item.nextElementSibling.classList.toggle('sec-list--actived-display')
+})
+cases_item.addEventListener('click', e => {
+  e.preventDefault()
+  cases_item.nextElementSibling.classList.toggle('sec-list--actived-display')
+})
