4bf2f792e021fe61af3476654a3eefa9bb2be07c
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-09-08 14:19:41 -0300

    create main hero responsive

--- js/header.js
+++ js/header.js
@@ -2,9 +2,11 @@ const btn_menu = document.querySelector('#btn-menu')
 const navbar = document.querySelector('.navbar')
 const solucoes_item = document.querySelector('#solucoes-item')
 const cases_item = document.querySelector('#cases-item')
+const rolagemBody = document.querySelector('body')
 btn_menu.addEventListener('click', e => {
   btn_menu.classList.toggle('navbar__btn-menu--actived')
   navbar.classList.toggle('navbar--actived')
+  rolagemBody.classList.toggle('not-overflow-y')
 })
 solucoes_item.addEventListener('click', e => {
   e.preventDefault()
